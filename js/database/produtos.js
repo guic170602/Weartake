56a2a3dcca14aace36d3a8fd8290e850ab0c73d1
guilherme <85708476+guic170602@users.noreply.github.com>
2022-02-02 14:41:49 -0300

    add imgs

--- js/database/produtos.js
+++ js/database/produtos.js
@@ -4,35 +4,35 @@ let produtosBd = [
         descricao : 'Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...',
         preco : 'R$ 100.00',
         categoria : '<i class="fas fa-tshirt"></i>Camisetas',
-        imagem : 'img/Men-Jacket-Front-Black.png'
+        imagem : './img/Men-Jacket-Front-Black.png'
     },
     {
         nome : 'Black Hat',
         descricao : 'O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...',
         preco : 'R$ 100.00',
         categoria : '<i class="fab fa-pied-piper-hat"></i>Acessórios',
-        imagem : 'img/gorro.png'
+        imagem : './img/gorro.png'
     },
     {
         nome : 'Mask',
         descricao : 'Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...',
         preco : 'R$ 40.00',
         categoria : '<i class="fab fa-pied-piper-hat"></i>Acessórios',
-        imagem : 'img/mascara.png'
+        imagem : './img/mascara.png'
     },
     {
         nome : 'T-Shirt',
         descricao : 'Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...',
         preco : 'R$ 100.00',
         categoria : '<i class="fas fa-tshirt"></i>Camisetas',
-        imagem : 'img/Men-TShirt-Black-Front.png'
+        imagem : './img/Men-TShirt-Black-Front.png'
     },
     {
         nome : 'Short-Sleeve T-Shirt',
         descricao : 'Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...',
         preco : 'R$ 100.00',
         categoria : '<i class="fas fa-tshirt"></i>Camisetas',
-        imagem : 'img/Men-TShirt-White.png'
+        imagem : './img/Men-TShirt-White.png'
     }
     ,
     {
@@ -40,6 +40,6 @@ let produtosBd = [
         descricao : 'Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...',
         preco : 'R$ 100.00',
         categoria : '<i class="fas fa-tshirt"></i>Camisetas',
-        imagem : 'img/Jacket.png'
+        imagem : './img/Jacket.png'
     }
 ]

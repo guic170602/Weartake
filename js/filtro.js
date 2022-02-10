e3be91b3b807434e579265934d37d58212d46dcc
guilherme <85708476+guic170602@users.noreply.github.com>
2022-02-10 16:57:34 -0300

    Fix Filter

--- js/filtro.js
+++ js/filtro.js
@@ -1,18 +1,20 @@
-function filtro(){
+function filtro() {
     const input = document.getElementsByClassName('pesquisa')
-    for(let index = 0; index < input.length; index++){
-            input[index].addEventListener('keypress',  e => {
-            mudaAtual(input[index].value)
-            const newProduto = produtosBd.filter(produto => produto.nome.toLowerCase().includes(input[index].value.toLowerCase()))
-            criaPrudutos(newProduto)
-            add(newProduto)
+    for (let index = 0; index < input.length; index++) {
+        input[index].addEventListener('keypress', e => {
+            if (e.key === 'Enter') {
+                mudaAtual(input[index].value)
+                const newProduto = produtosBd.filter(produto => produto.nome.toLowerCase().startsWith(input[index].value.toLowerCase()))
+                criaPrudutos(newProduto)
+                add(newProduto)
+            }
         })
     }
     const pesquisa = document.getElementsByClassName('pesquisar')
-    for(let index = 0; index<pesquisa.length; index++){
-        pesquisa[index].addEventListener('click', ()=>{
+    for (let index = 0; index < pesquisa.length; index++) {
+        pesquisa[index].addEventListener('click', () => {
             mudaAtual(input[index].value)
-            const newProduto = produtosBd.filter(produto => produto.nome.toLowerCase().includes(input[index].value.toLowerCase()))
+            const newProduto = produtosBd.filter(produto => produto.nome.toLowerCase().startsWith(input[index].value.toLowerCase()))
             criaPrudutos(newProduto)
 
             add(newProduto)
@@ -20,34 +22,34 @@ function filtro(){
     }
 
     const todos = document.getElementById('todos')
-    todos.addEventListener('click', ()=>{
+    todos.addEventListener('click', () => {
         mudaAtual('todos')
         const newProduto = produtosBd
         criaPrudutos(newProduto)
 
         add(newProduto)
     })
-    
+
     const acessorios = document.getElementById('acessorios')
-    acessorios.addEventListener('click', ()=>{
+    acessorios.addEventListener('click', () => {
         mudaAtual('acessorios')
         const newProduto = produtosBd.filter(produto => produto.categoria.toLowerCase().includes('acessórios'))
         criaPrudutos(newProduto)
 
         add(newProduto)
     })
-    
+
     const calçados = document.getElementById('calçados')
-    calçados.addEventListener('click', ()=>{
+    calçados.addEventListener('click', () => {
         mudaAtual('calçados')
         const newProduto = produtosBd.filter(produto => produto.categoria.toLowerCase().includes('calçados'))
         criaPrudutos(newProduto)
 
         add(newProduto)
     })
-    
+
     const camisetas = document.getElementById('camisetas')
-    camisetas.addEventListener('click', ()=>{
+    camisetas.addEventListener('click', () => {
         mudaAtual('camisetas')
         const newProduto = produtosBd.filter(produto => produto.categoria.toLowerCase().includes('camisetas'))
         criaPrudutos(newProduto)
@@ -56,13 +58,13 @@ function filtro(){
     })
 }
 
-function mudaAtual(atual){
+function mudaAtual(atual) {
     const filtro = document.getElementsByClassName('filtro')
     const id = document.getElementById(atual)
-    for(let index = 0; index<filtro.length; index++){
-        if(filtro[index] == id){
+    for (let index = 0; index < filtro.length; index++) {
+        if (filtro[index] == id) {
             filtro[index].classList.add('atual')
-        }else{
+        } else {
             filtro[index].classList.remove('atual')
         }
     }
